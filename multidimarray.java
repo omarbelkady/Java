96d96ef56d1b93679c82f3d6de00a43bfaa1cc09
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-08 23:14:44 +0100

    Create multidimarray.java
    We can use arrays to store a list of objects. I can create multi-dimensional arrays. I can create a 2d array to store a matrix.

---
+++ multidimarray.java
@@ -0,0 +1,11 @@
+package com.amneziahookahlounge
+import java.utils.Arrays
+	
+public class Main{
+	public static void main(String [] args)
+	{
+		
+		//Multidimensional Array Declaration 2 rows by 3 columns
+		int [][] funNumsPart2= {{1,2,3},{4,5,6}};
+	}
+}

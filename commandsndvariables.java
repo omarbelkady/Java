295b4a64f566b1fe8b8a84f62fd04212352b4360
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-08 22:38:06 +0100

    Create commandsndvariables.java

---
+++ commandsndvariables.java
@@ -0,0 +1,11 @@
+package com.amneziahookahlounge;
+
+
+public class Main{
+		public static void main(String [] args)
+    {
+			//declaration of variable
+			Integer age=26;
+      System.out.println(age);
+		}
+}

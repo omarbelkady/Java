df2e2a2e6387f5e73e35156eb7f848895861ef62
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-08 23:26:09 +0100

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,15 @@
+package com.omarbelkady;
+
+public class Main{
+	public static void main(String [] args)
+	{
+		//the round method in the math class is overloaded depending on the input
+		//if I input a float it returns a double
+		//if I input a double it returns a long
+		int result = Math.round(1.1F);
+		int result2= (int)Math.ceil(1.1F);
+		//result2 operation performs a ceiling to the value meaning round it up
+    System.out.println(result);
+	}
+
+}

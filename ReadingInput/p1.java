8fc1f6eb85e71e6b4005db09fbf5c7961445f4c2
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-08 23:42:00 +0100

    Create p1.java

---
+++ ReadingInput/p1.java
@@ -0,0 +1,21 @@
+package com.omarbelkady;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String [] args)
+    {
+      //Creating a scanner object. The parameters we pass in, specifies where we 
+      //will read user input from: terminal window/file
+      //Terminal window: System.in
+      //File
+      Scanner scanner= new Scanner(System.in);
+      byte age= scanner.nextByte();//for reading the next byte and storing it in a var
+      //scanner.nextLine();//for reading the next line aka getline(cin, varname) in C++
+      //scanner.nextBoolean();//for reading next boolean
+
+      //line below is implicit casting or implicit type conversion
+      //I am converting the age of type byte to a string to have the string printed 
+      //out to the console
+      System.out.println("My friend you are: " + age);
+    }

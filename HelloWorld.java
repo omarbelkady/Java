882a39327ec52420037c155c4bc355a4d11ef139
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-09 01:50:41 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
 package com.domainname;
 
 
-public class Main{
-		public static void main(String [] args) {
-			System.out.println("Hello World");
-		}
+public class HelloWorld{
+	public static void main(String [] args) {
+		System.out.println("Hello World");
+	}
 }

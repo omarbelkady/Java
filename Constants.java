0b45ac7b1d5aea8382197fda0a16c585a9ff7ae9
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-09 01:48:43 +0100

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,10 +1,10 @@
 package com.domainName;
 import java.utils.Arrays
 	
-public class Main{
+public class Constants{
 	public static void main(String [] args)
 	{
-    //the final keyword enforces immutability on the variable
+    		//the final keyword enforces immutability on the variable
 		final float PI=3.141592653F;
 	}
 }

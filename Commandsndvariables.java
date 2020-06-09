abb08b129c24d3ba3ea16ec221a1dfd1e245a151
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-09 01:46:42 +0100

    Update Commandsndvariables.java

--- Commandsndvariables.java
+++ Commandsndvariables.java
@@ -1,11 +1,12 @@
-package com.amneziahookahlounge;
+package com.omarbelkady;
 
 
-public class Main{
-		public static void main(String [] args)
-    {
-			//declaration of variable
-			Integer age=26;
-      System.out.println(age);
-		}
+public class Commandsndvariables
+{
+	public static void main(String [] args)
+    	{
+		//declaration of variable
+		Integer age=26;
+      		System.out.println(age);
+	}
 }

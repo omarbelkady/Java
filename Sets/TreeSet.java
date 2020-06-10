e3756912ff6c4aee38ca5a33ab775d4d51d75848
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-10 16:56:29 +0100

    Create TreeSet.java

---
+++ Sets/TreeSet.java
@@ -0,0 +1,39 @@
+//A tree set is ordered in a tree data structure
+//TREE SET
+//package tutorial;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+public class Main
+{
+    //main method
+    public static void main (String[] args)
+    {
+        //Syntax: Set<Datatype> nameofSet = new TreeSet<datatype>()
+        Set<Integer> t = new TreeSet<Integer>();
+        
+        //To append to a Set
+        t.add(5);
+        t.add(8);
+        t.add(5);
+        t.add(9);
+        System.out.println(t);//print 5, 8, 9
+        t.add(-8);
+        t.remove(8);
+        
+        //clear an entire Set
+        //t.clear();
+        
+        //check if empty
+        //t.isEmpty()
+        
+        //get the length of the Set
+        //t.size();
+        
+        //Check if element exists within the Set
+        boolean m = t.contains(5);
+        System.out.println(m);
+        System.out.println(t);
+        
+    }
+}

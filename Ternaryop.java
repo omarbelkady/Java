2c446bef68337933a43bfdcb7d861cbdf18f775a
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-11 18:33:32 +0100

    Create Ternaryop.java

---
+++ Ternaryop.java
@@ -0,0 +1,11 @@
+package com.omarbelkady;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Integer income = 120000;
+	    String className= income > 100000 ? "First":"Economy" ;
+	    //className=condition?true:false
+		System.out.println(className);
+	}
+}

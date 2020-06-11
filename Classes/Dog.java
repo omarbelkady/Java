e2978f95e7271e033f15f4c5eeb1cc03eec93428
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-11 15:11:26 +0100

    Create Dog.java

---
+++ Classes/Dog.java
@@ -0,0 +1,39 @@
+public class Dog
+{
+    private String name;
+    private int age;
+    
+    //Let's create a constructor
+    public Dog(String name, int age){
+        //set the age and attributes to whatever we pass in
+        this.name=name;
+        this.age=age;
+        
+        //The this keyword is referencing the attributes of the class
+        
+        add2();
+        speak();
+    }
+    
+    public void speak()
+    {
+        System.out.println("I am "+this.name+" and I am "+ this.age+ " years old");
+    }
+    
+    //Lets create another method to get the age
+    public int getAge()
+    {
+        return this.age;
+    }
+    
+    //Lets create another method that will set the age
+    public void setAge(int age)
+    {
+        this.age=age;
+    }
+    
+    private int add2()
+    {
+        return this.age+2;
+    }
+}

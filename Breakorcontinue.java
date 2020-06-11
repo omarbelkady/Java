014ec11bbf8753fe9f6b043e8c3afcd3b387146c
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-11 18:40:25 +0100

    Create Breakorcontinue.java

---
+++ Breakorcontinue.java
@@ -0,0 +1,30 @@
+### The break statement terminates the loop and the continue statement restarts at the beginning of the loop
+package com.omarbelkady;
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    
+	    Scanner scanner= new Scanner( System.in );
+	    
+	    String input="1";
+	    
+	    while(true)
+	    {
+	        System.out.print("Input: ");
+	        input=scanner.next().toLowerCase();
+	        if(input.equals("pass"))
+	            continue;
+	        if(input.equals("quit"))
+	            break;
+	        System.out.println(input);
+	    }
+	    
+	    
+	   
+	}
+
+}

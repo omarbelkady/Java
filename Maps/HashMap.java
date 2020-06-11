08f2e3e3e6ee0376ce6a8cfc89414e8a68e14fb2
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-11 14:55:35 +0100

    Create HashMap.java

---
+++ Maps/HashMap.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Main
+{
+    public static void main (String[] args)
+    {
+        Map m = new HashMap();	
+    }
+}

81a3555ec18fae7aac407ef19f4c96be5804a04a
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-11 14:46:02 +0100

    Create ConditionalsWthCompOp.java

---
+++ ConditionalsWthCompOp.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    //main method
+    public static void main (String[] args)
+    {
+        int x= 7;
+        int y=9;
+        int z=12;
+        boolean compare;
+        //Compare: ==, >=,<=, <, >, !=
+        //Assign: =
+        compare= x<y;
+        
+        System.out.println("Hello");
+        System.out.println(compare);
+        
+    }
+}

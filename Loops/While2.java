abb0ca60e18548cc0ffdc5cae440e951868ee678
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-17 23:27:51 +0100

    Create While2.java

---
+++ Loops/While2.java
@@ -0,0 +1,12 @@
+public class While2
+{
+   	public static void main (String[] args) 
+    {
+        int myIter = 0;
+        while(myIter < 10)
+        {
+            System.out.println("This is my iter bro " + myIter);
+            myIter++;
+        }
+    }
+}

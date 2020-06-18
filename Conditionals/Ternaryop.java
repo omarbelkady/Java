9af289ad61b1eafd8580dd375d37de27f9050278
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-18 15:11:34 +0100

    Update Ternaryop.java

--- Conditionals/Ternaryop.java
+++ Conditionals/Ternaryop.java
@@ -1,11 +1,11 @@
 package com.omarbelkady;
 
-public class Main
+public class Ternaryop
 {
 	public static void main(String[] args) {
 		Integer income = 120000;
-	    String className= income > 100000 ? "First":"Economy" ;
-	    //className=condition?true:false
+	    	String className= income > 100000 ? "First":"Economy" ;
+	    	//className=condition?true:false
 		System.out.println(className);
 	}
 }

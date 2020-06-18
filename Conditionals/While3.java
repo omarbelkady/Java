d032ed1a18be240f83d61f559f947802ea9e5514
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-18 15:07:06 +0100

    Update and rename Loops/While.java to Conditionals/While3.java

--- Conditionals/While3.java
+++ Conditionals/While3.java
@@ -2,7 +2,7 @@ package com.omarbelkady;
 import java.util.Scanner;
 
 
-public class Main
+public class While3
 {
 	public static void main(String[] args) {
 	    Scanner scanner= new Scanner(System.in);

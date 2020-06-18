effa5895d1ad1921e4217a34660ae561190f07fa
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-18 12:19:53 +0100

    Update ReferenceType3.java

--- ReferenceType3.java
+++ ReferenceType3.java
@@ -1,14 +1,13 @@
-package com.amneziahookahlounge
-	
+package com.domainName;
 import java.awt.*;
 	
-public class Main{
+public class ReferenceType3{
 	public static void main(String [] args)
 	{
 		System.out.println("now: "+now);
 		Point point1= new Point(x:1,y:1);//allocate mem to store the point obj
 		Point point 2=point1;//allocate mem to store the point obj reference to the point object in memory
-    byte x=1;
+    		byte x=1;
 		byte y=x;
 		//x and y are at different memory locations
 		x=3;

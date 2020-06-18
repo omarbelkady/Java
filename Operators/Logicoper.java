32f3f47fc38e82d1d0f7fc24f150da0fa020ccac
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-18 12:07:23 +0100

    Update and rename operators/logicoper.java to Operators/Logicoper.java

---
+++ Operators/Logicoper.java
@@ -0,0 +1,10 @@
+public class Logicoper
+{
+
+        public static void main(String[] args) 
+        {
+              int temperature=22;
+              boolean isCool= temperature > 20 && temperature <30;
+              System.out.println(isCool);//output:true
+        }
+}

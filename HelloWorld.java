660c992b571f9149d38d70be1ab0a56bc9d44b86
alan <alan@msi.loc>
2020-06-19 12:46:41 -0500

    able to compile and run on vscode, don't use packages unless the file is in the folder

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,8 @@
-package com.domainname;
-
 
 public class HelloWorld{
-	public static void main(String [] args) {
+	public static void main(String [] args) 
+	{
+		System.out.println("252609274373");
 		System.out.println("Hello World");
 	}
 }

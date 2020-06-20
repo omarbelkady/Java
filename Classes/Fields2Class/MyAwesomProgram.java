588a703287713457cebcc566a9a32a660758ff60
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-20 11:24:06 +0100

    Update MyAwesomProgram.java

--- Classes/Fields2Class/MyAwesomProgram.java
+++ Classes/Fields2Class/MyAwesomProgram.java
@@ -9,3 +9,25 @@ public class MyAwesomeProgram
 		user.lastName= "Smith";
 	}
 }
+
+
+
+//<----------------------------------CHANGING THE ABOVE CODE TO THE BOTTOM CODE TO CALL THE OUTPUT METHOD ON USER!------------------------------------------->
+public class MyAwesomeProgram
+{
+	public static void main(String [] args)
+	{
+		User user = new User();//not a field not a class but a loc var to the main meth
+		user.firstName= "Bob";
+		user.lastName= "Smith";
+		user.output(7);
+		//prints:
+		//Bob Smith
+		//Bob Smith
+		//Bob Smith
+		//Bob Smith
+		//Bob Smith
+		//Bob Smith
+		//Bob Smith
+	}
+}

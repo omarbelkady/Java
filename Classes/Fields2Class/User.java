a02922b6e6fd5a9c1d10d15bf32fe4e3eaa64324
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-20 11:30:51 +0100

    Update User.java

--- Classes/Fields2Class/User.java
+++ Classes/Fields2Class/User.java
@@ -1,11 +1,34 @@
+//<---------------------------STEP1:DEFINE THE FIELDS------------------------------>
+/*
+public class User
+{
+    public String firstName;
+    public String lastName;
+}
+
+
+*/
+
+//<--------STEP2:DEFINE THE METHODS WITH THE APPROPRIATE RETURN TYPE AS A KEYWORD PLACED BEFORE THE NAME AND AFTER THE ACCESS MODIFIER-------->
 public class User
 {
 	public String firstName;
 	public String lastName;
 
-	public void output()
-	{
-		System.out.println(firstName + " " + lastName);
-	}
 
+	/* The bottom code will error because we declared it as a void meaning
+		We do not want anything to be returned to us*/
+	/*
+	public void output(){
+		return "Hi, my name is "+ firstName + " " + lastName + ".";
+	}
+	*/
+
+	public String output()
+	{
+		return "Hi, my name is "+ firstName + " " + lastName + '.";
+	}
 }
+
+
+

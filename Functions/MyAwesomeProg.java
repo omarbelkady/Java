6ebd896ea8571a5095b11c7e19090d532865a7d9
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-21 13:07:27 +0100

    Create MyAwesomeProg.java

---
+++ Functions/MyAwesomeProg.java
@@ -0,0 +1,12 @@
+public class MyAwesomeProg
+{
+	public static void main(String [] args)
+	{
+		User user = new User();
+		user.setFirstName("Bob");
+		user.setLastName("Smith");
+		
+        System.out.println(user.getFullName());	
+
+	}
+}

9010d0b5849c1d35014d616945c738db188ab757
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-23 23:43:41 +0100

    Create MyAwesomeProg.java

---
+++ Inheritance/MyAwesomeProg.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class MyAwesomeProg
+{
+	public static void log(Object o)
+   	{       
+   		System.out.print(o);
+	}
+
+	public static void logln(Object o)
+	{
+   		System.out.println(o);
+	}
+    
+	public static void main(String [] args)
+	{
+		
+
+	}
+}

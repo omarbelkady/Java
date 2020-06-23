59b22ad8254ef61bbaa09deb512086547e4a2852
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-23 23:45:45 +0100

    Create MyAwesomeProg.java

---
+++ Inheritance/p2/MyAwesomeProg.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class MyAwesomeProg
+{
+	public static void log(Object o)
+   	{       
+   		System.out.print(o);
+	}
+
+	public static void logln(Object o)
+	{
+   		System.out.println(o);
+	}
+    
+	public static void main(String [] args)
+	{
+		Student m = new Student();
+		m.major = “Computer Science”;
+		logln(m.major);
+	}
+}

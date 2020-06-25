fbf5cc31e0eeffd46211f15b63c0db2b67919977
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-25 19:45:14 +0100

    Create User.java

---
+++ ParentClassMethodsPLUSSuper/User.java
@@ -0,0 +1,21 @@
+public abstract class User
+{
+	public String firstName;//fieldA
+	public String lastName;//fieldB
+	public boolean verified = false;
+	
+	public static void log(Object o)
+   	{
+		System.out.print(o);
+	}
+	
+	public static void logln(Object o)
+	{
+		System.out.println(o);
+	}
+
+	public void sayHello()
+	{
+		logln("User Type");
+	}	
+}

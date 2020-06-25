cc1cee759dc3ff9bf661ad1089e467707d219cd1
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-25 19:49:25 +0100

    Update User.java

--- ParentClassMethodsPLUSSuper/User.java
+++ ParentClassMethodsPLUSSuper/User.java
@@ -1,21 +1,41 @@
 public abstract class User
 {
-	public String firstName;//fieldA
-	public String lastName;//fieldB
-	public boolean verified = false;
+	private String firstName;//fieldA
+	private String lastName;//fieldB
 	
-	public static void log(Object o)
+    	public static void log(Object o)
    	{
-		System.out.print(o);
-	}
+   	    System.out.print(o);
+
+   	}
 	
-	public static void logln(Object o)
+    	public static void logln(Object o)
 	{
-		System.out.println(o);
+	    System.out.println(o);
 	}
 
-	public void sayHello()
+	//Remember this is an abstract class which means
+	//we will not be invoking the constructor directly
+	//Which means to not call it within our calling program
+	//But in our Student Class
+	//I create a User Constructor
+	public User(String fName, String lName)
 	{
-		logln("User Type");
+		//Remember fieldName=parameterName
+		//NOTTT parameterName=fieldName
+		firstName=fName;
+		lastName=lName;
+
+
+	}
+
+	public final void sayHello()
+	{
+	    logln(firstName+ " "  + lastName);
 	}	
 }
+
+    //Remember this is an abstract class which means
+    //we will not be invoking the constructor directly
+    //Which means to not call it within our calling program
+    //But in our Student Class

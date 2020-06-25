7e627aa05fddb32c949bf50bef6330bb907c1310
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-25 14:49:01 +0100

    Create Student.java

---
+++ Polymorphism/Student.java
@@ -0,0 +1,18 @@
+public class Student extends User
+{
+	public boolean verified = true;//method overriding
+	public String major;
+	
+	
+	
+	
+	/*my Student class which is the derived class I must create a 
+	method called sayHello which will override the method because it is necessary or 
+	else this will raise an error.*/
+	@Override
+	public void sayHello()
+	{
+		logln("Hello y\'all my major is "+ major+
+		". My name is " + firstName + " " + lastName);
+	}
+}

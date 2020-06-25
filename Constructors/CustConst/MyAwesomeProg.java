07d64e5fd248c83a56e06132c4f33b21f309c910
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-25 16:45:11 +0100

    Create MyAwesomeProg.java

---
+++ Constructors/CustConst/MyAwesomeProg.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class MyAwesomeProg
+{
+	public static void log(Object o)
+	{       
+		System.out.print(o);
+	}
+
+	public static void logln(Object o)
+	{
+		System.out.println(o);
+	}
+    
+	public static void main(String [] args)
+	{
+		//This is not possible I have to create a constructor that takes two arguments
+		//Not 1
+		//Student d = new Student("Driss", "PUT THE LNAME HEERE")
+		Student f = new Student("Kenza", "Saleh");
+	}
+}
